de7697573691775e58c94a21189464e5c205dca2
ocean22092004 <vthadaiduong@gmail.com>
2026-06-06 23:29:56 +0700

    1

---
+++ client/src/shared-kernel/presentation/components/NuxtUiProvider.vue
@@ -0,0 +1,7 @@
+<!-- English description: Provides Nuxt UI application context and toast host for routes that use Nuxt UI runtime components. -->
+<template>
+  <UApp>
+    <slot />
+    <UToaster />
+  </UApp>
+</template>
